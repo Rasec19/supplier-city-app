9b3e1f9f736b11b334ae1923cdd590c7954b1dd9
Rasec Camacho <rasec.camacho19@gmail.com>
2024-04-16 14:04:33 -0600

    primer diseño supplier app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,183 @@
 import { Component } from '@angular/core';
+interface IStory {
+  vacations: string,
+  current: string,
+  programated: string,
+  available: string,
+  period: string,
+}
 
+interface IHistory {
+  dates: string,
+  politycs: string,
+  descripcion: string,
+  days: string,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'supplier-city-app';
+  title = 'Vacaciones';
+  public stories: IStory[] = [
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+    {
+      vacations: '100',
+      current: '100',
+      programated: '100',
+      available: '100',
+      period: '100',
+    },
+  ];
+  public histories: IHistory[] = [
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+    {
+      dates: '101',
+      politycs: '101',
+      descripcion: '101',
+      days: '101',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TableModule } from 'primeng/table';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TableModule,
+    CardModule,
+    MenubarModule,
+    AvatarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
